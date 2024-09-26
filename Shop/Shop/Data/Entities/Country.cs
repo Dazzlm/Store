2ea7c193dfa58fdd7fe13e2dddf4678936218be5
Daniel Felipe Marin Marin <danielmarin1116044@correo.itm.edu.co>
2024-09-26 15:11:53 -0500

    BD and Crud

---
+++ Shop/Shop/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Data.Entities
+{
+    public class Country
+
+
+    {
+
+
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El Campo{0} debe tener maximo {1} caractéres.")]
+        [Required(ErrorMessage =  "El Campo{0} es obligatorio.")]
+        public String Name { get; set; }
+    }
+}
